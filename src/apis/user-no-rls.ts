4e4adef650ed6c00a2709609dacef856a90b1488
bzzoo <fullbster@gmail.com>
2024-12-31 01:16:13 +0900

    feat: 제안현황 페이크 api 추가가

--- src/apis/user-no-rls.ts
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { createSupabaseBrowserClient } from '@/supabase/supabase-client'
-
-export const getUser = async (id: string) => {
-  const supabase = createSupabaseBrowserClient()
-}
